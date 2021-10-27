493835326e77a8086f0e1b894354232e124615f1
June Kelly <june.kelly@overleaf.com>
2021-10-27 10:50:06 +0100

    code mirror 6 init

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+import {EditorState} from "@codemirror/state"
+import {EditorView, keymap} from "@codemirror/view"
+import {defaultKeymap} from "@codemirror/commands"
+
 console.log(">> Init")
+
+let startState = EditorState.create({
+  doc: "Hello World",
+  extensions: [keymap.of(defaultKeymap)]
+})
+
+let view = new EditorView({
+  state: startState,
+  parent: document.querySelector('#editor')
+})
