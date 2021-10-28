d6f467011d9b3a58fdd5ca90858b49049276a058
June Kelly <june.kelly@overleaf.com>
2021-10-28 13:13:52 +0100

    update, decoration example

--- app.js
+++ app.js
@@ -1,8 +1,57 @@
 import { basicSetup, EditorState, EditorView } from '@codemirror/basic-setup';
+import { keymap, Decoration, DecorationSet } from "@codemirror/view"
+import { StateField, StateEffect } from "@codemirror/state"
 
+// Underline command
+const addUnderline = StateEffect.define()
+
+const underlineField = StateField.define({
+  create() {
+    return Decoration.none
+  },
+  update(underlines, tr) {
+    underlines = underlines.map(tr.changes)
+    for (let e of tr.effects) if (e.is(addUnderline)) {
+      underlines = underlines.update({
+        add: [underlineMark.range(e.value.from, e.value.to)]
+      })
+    }
+    return underlines
+  },
+  provide: f => EditorView.decorations.from(f)
+})
+
+const underlineMark = Decoration.mark({class: "cm-underline"})
+
+const underlineTheme = EditorView.baseTheme({
+  ".cm-underline": { textDecoration: "underline 3px red" }
+})
+
+export function underlineSelection(view) {
+  console.log(">>>> underline selection")
+  let effects = view.state.selection.ranges
+    .filter(r => !r.empty)
+    .map(({from, to}) => addUnderline.of({from, to}))
+  console.log("  - effects: ", effects)
+  if (!effects.length) return false
+
+  if (!view.state.field(underlineField, false))
+    effects.push(StateEffect.appendConfig.of([underlineField,
+                                              underlineTheme]))
+  view.dispatch({effects})
+  return true
+}
+
+export const underlineKeymap = keymap.of([{
+  key: "Mod-h",
+  preventDefault: true,
+  run: underlineSelection
+}])
+
+// Setup
 let startState = EditorState.create({
   doc: "Hello World",
-  extensions: [basicSetup]
+  extensions: [basicSetup, underlineKeymap]
 })
 
 let view = new EditorView({
